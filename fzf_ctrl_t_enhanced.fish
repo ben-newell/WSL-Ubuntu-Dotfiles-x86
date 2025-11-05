40598944a02c041a5de2c310f609aa067f3aa29d
ben-newell <bennewell.mail@gmail.com>
2025-11-05 13:23:55 -0600

    [add] recursion to enhanced ctrl+t fd implementation

--- fzf_ctrl_t_enhanced.fish
+++ fzf_ctrl_t_enhanced.fish
@@ -1,7 +1,8 @@
 function fzf_ctrl_t_enhanced
-    set -l search_cmd (set -q FZF_CTRL_T_COMMAND; and echo $FZF_CTRL_T_COMMAND; or echo "fd --color always")
-    
-    # Complete FZF options (don't append user opts)
+    # Use full-path search to include nested directories
+    set -l search_cmd "fd --type f --hidden --no-ignore --exclude .git --color always --full-path ."
+
+    # FZF options
     set -l fzf_opts --ansi --multi \
         --preview 'bat --color=always --style=numbers --line-range=:200 {}' \
         --preview-window=right:60% \
@@ -9,18 +10,18 @@ function fzf_ctrl_t_enhanced
         --bind 'ctrl-y:execute-silent(echo {} | clip.exe)+abort' \
         --bind 'ctrl-e:execute(nvim {})' \
         --header 'Ctrl-Y: Copy | Ctrl-E: Edit | Ctrl-A: Select All'
-    
-    # REMOVED: if set -q FZF_CTRL_T_OPTS block
-    
+
+    # Run search + fzf
     set -l selected (eval $search_cmd | fzf $fzf_opts)
-    
+
+    # Handle selected file(s)
     if test -n "$selected"
         if functions -q fzf_ctrl_t_post_cmd
             fzf_ctrl_t_post_cmd $selected
         end
-        
         commandline -i (string escape -- $selected | string join ' ')
     end
-    
+
     commandline -f repaint
 end
+
